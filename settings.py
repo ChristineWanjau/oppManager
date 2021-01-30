19691026040f6b6cf6eba88ff902a221ff676932
ChristineWanjau <62501578+ChristineWanjau@users.noreply.github.com>
2021-01-30 10:13:47 +0300

    changed

--- settings.py
+++ settings.py
@@ -170,10 +170,10 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['opportunity-manager.herokuapp.com']
+ALLOWED_HOSTS = ['opportunity-manager.herokuapp.com','127.0.0.1']
